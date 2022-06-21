2734a0ce19bc80c6def36c3f73c20ab1fdf4d31c
amansharmaria <107911646+amansharmaria@users.noreply.github.com>
2022-06-21 12:20:18 +0530

    Update App.java

--- src/main/java/com/ria/example/App.java
+++ src/main/java/com/ria/example/App.java
@@ -1,21 +1,6 @@
 package com.ria.example;
 
-/**
- * Hello world!
- * This class converts arabic numbers to roman numbers upto a 100
- * Here's a brief reminder of Roman numbers
- * I - 1
- * II - 2
- * IV - 4
- * V - 5
- * VI - 6
- * IX - 9
- * X - 10
- * XL - 40
- * L - 50
- * XC - 90
- * C - 100
- */
+
 public final class App {
     private App() {
     }
