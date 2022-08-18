e0adae44617b19f391fded9b4321d8502e91d283
Aman Verma <53325551+AmanVerma13@users.noreply.github.com>
2022-08-18 12:19:05 +0530

    aman verma ticktock app code
    aman verma ticktock app code

--- src/main/java/com/ria/example/App.java
+++ src/main/java/com/ria/example/App.java
@@ -1,6 +1,8 @@
 package com.ria.example;
 
 
+import java.util.Scanner;
+
 public final class App {
     private App() {
     }
@@ -11,5 +13,45 @@ public final class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        char tickTack[] = new char[9];
+
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            char ch = sc.nextLine().charAt(0);
+            int index = sc.nextInt();
+            if (ch == 'X'){
+                tickTack[index] = 'X';
+            }
+            if (ch == 'Y'){
+                tickTack[index] = 'Y';
+            }
+            if(checkLineMatch(ch, index, tickTack)){
+                System.out.println("Winner");
+                break;
+            }
+        }
+
     }
+    private static boolean checkLineMatch(char ch, int index, char tickTack[]){
+        int[][] winn1 = {{1,2,3}, {1,4,7}, {1,5,9}, {2,5,8}, {3,6,9}, {3,5,7}, {4,5,6}, {7,8,9}};
+
+        for (int i =0; i< 3; i++){
+            boolean found = true;
+            for (int j=0; j<9; j++){
+                int charIdx = winn1[i][j];
+                if (tickTack[charIdx] != ch){
+                    found = false;
+                    break;
+                }
+            }
+            if (!found){
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
