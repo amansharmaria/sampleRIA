c8378c6247534ef9f901a60b9382eb6ff6a5cf8e
amansharmaria <107911646+amansharmaria@users.noreply.github.com>
2022-09-29 17:43:09 +0530

    Update App.java

--- src/main/java/com/ria/example/App.java
+++ src/main/java/com/ria/example/App.java
@@ -1,15 +1,16 @@
 package com.ria.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public final class App {
-    private App() {
-    }
+@SpringBootApplication
+public class App {
 
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
